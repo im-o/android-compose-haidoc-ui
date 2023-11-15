ceb7296665230f30debc52acbe9ab2e8bd2a9a28
rivaldy <frizgt@gmail.com>
2023-11-16 00:31:52 +0800

    :label: add resources and update theme - string, dimens, theme, color

---
+++ core/src/main/kotlin/id/rivaldy/core/util/Dimens.kt
@@ -0,0 +1,40 @@
+package id.rivaldy.core.util
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/** Created by github.com/im-o on 11/15/2023. */
+
+object Dimens {
+    val dp0: Dp = 0.dp
+    val dp1: Dp = 1.dp
+    val dp3: Dp = 3.dp
+    val dp4: Dp = 4.dp
+    val dp6: Dp = 6.dp
+    val dp8: Dp = 8.dp
+    val dp10: Dp = 10.dp
+    val dp12: Dp = 12.dp
+    val dp16: Dp = 16.dp
+    val dp24: Dp = 24.dp
+    val dp20: Dp = 20.dp
+    val dp28: Dp = 28.dp
+    val dp32: Dp = 32.dp
+    val dp38: Dp = 38.dp
+    val dp40: Dp = 40.dp
+    val dp48: Dp = 48.dp
+    val dp60: Dp = 60.dp
+    val dp80: Dp = 80.dp
+    val dp100: Dp = 100.dp
+    val dp120: Dp = 120.dp
+    val dp140: Dp = 140.dp
+    val dp150: Dp = 150.dp
+
+    val sp12: TextUnit = 12.sp
+    val sp14: TextUnit = 14.sp
+    val sp16: TextUnit = 16.sp
+    val sp24: TextUnit = 24.sp
+    val sp32: TextUnit = 32.sp
+    val sp40: TextUnit = 40.sp
+}
