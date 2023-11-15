5a224c2dfa38090c8f85c97c5f42c9b63d4359b1
rivaldy <frizgt@gmail.com>
2023-11-15 11:04:49 +0800

    :tada: initial project - fixed build.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    // Warning @Incubating cause on progress
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Compose HaiDoc"
+include(":app")
+ 
