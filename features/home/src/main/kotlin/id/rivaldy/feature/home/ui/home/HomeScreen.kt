bdee0324248202141db30fa0f5588e2a8be2257a
rivaldy <frizgt@gmail.com>
2023-11-16 16:20:54 +0800

    :bug: need-fixed, add thanks on home

--- features/home/src/main/kotlin/id/rivaldy/feature/home/ui/home/HomeScreen.kt
+++ features/home/src/main/kotlin/id/rivaldy/feature/home/ui/home/HomeScreen.kt
@@ -1,11 +1,28 @@
 package id.rivaldy.feature.home.ui.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import id.rivaldy.core.util.Dimens
 
 /** Created by github.com/im-o on 11/15/2023. */
 
 @Composable
-fun HomeScreen() {
-    Text(text = "Hello from home")
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(Dimens.dp60),
+    ) {
+        Text(
+            modifier = modifier.align(Alignment.Center),
+            text = "Halaman Home [on progress], Profile pun [on progress]. maaf kehabisan waktu, terimakasih."
+        )
+    }
 }
